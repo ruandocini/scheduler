e63f8debd2066dee9cef4793f8c9f6435be241a1
Lucas D Nery <nerlucas@amazon.com>
2022-10-21 01:09:50 -0300

    Corrige ConcurrentModificationException ao manipular items da blocked queue através de enhanced for-each

--- src/main/java/org/example/scheduler/structures/BlockedQueue.java
+++ src/main/java/org/example/scheduler/structures/BlockedQueue.java
@@ -6,6 +6,7 @@ import org.example.scheduler.process.ProcessControlBlock;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -20,11 +21,13 @@ public class BlockedQueue {
         }
 
         List<ProcessControlBlock> unblockedProcesses = new ArrayList<>();
-        for (BlockedProcess blockedProcess : blockedQueue) {
+        Iterator<BlockedProcess> blockedProcessIterator = blockedQueue.iterator();
+        while (blockedProcessIterator.hasNext()) {
+            BlockedProcess blockedProcess = blockedProcessIterator.next();
             blockedProcess.decreaseBlockedPeriod();
 
             if (blockedProcess.blockedPeriod == 0) {
-                blockedQueue.remove(blockedProcess);
+                blockedProcessIterator.remove();
                 unblockedProcesses.add(blockedProcess.getProcessControlBlock());
             }
         }
